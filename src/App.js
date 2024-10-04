786a0b8e726f10be287add17b6bf2e344a172e23
elizabethvilloria <lizvilloria@gmail.com>
2024-10-04 16:42:10 +0200

    Add navigation menu with links to Home and Booking pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BookingPage from './pages/BookingPage';
 
 function App() {
     return (
         <Router>
+            <nav>
+                <Link to="/">Home</Link> | <Link to="/booking">Booking</Link>
+            </nav>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/booking" element={<BookingPage />} />
