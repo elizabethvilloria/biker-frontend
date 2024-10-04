81157b8509ee66e8a61c34b8cf8a518a44e0a7c4
elizabethvilloria <lizvilloria@gmail.com>
2024-10-04 16:41:48 +0200

    Add basic routing and pages for Home and Booking

---
+++ src/pages/BookingPage.js
@@ -0,0 +1,6 @@
+// src/pages/BookingPage.js
+function BookingPage() {
+    return <h1>Book a Bike</h1>;
+}
+
+export default BookingPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,6 @@
+// src/pages/HomePage.js
+function HomePage() {
+    return <h1>Welcome to Bike Booking</h1>;
+}
+
+export default HomePage;
