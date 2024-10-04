9404e436dabf1cd7d41b512f6c068325f64a3ab8
elizabethvilloria <lizvilloria@gmail.com>
2024-10-04 17:00:46 +0200

    Image rendered successfully

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,12 @@
-// src/pages/HomePage.js
+import React from 'react';
+
 function HomePage() {
     return (
         <div>
             <h1>Welcome to Bike Booking</h1>
             <div>
                 <h2>Our Bike</h2>
-                <img src="your-bike-image-url" alt="Our Bike" style={{ width: '300px', height: 'auto' }} />
+                <img src="/images/bike.png" alt="Our Bike" style={{ width: '300px', height: 'auto' }} />
                 <p>Description of the bike. For example, "A high-quality mountain bike perfect for trails and rough terrain."</p>
                 <button onClick={() => window.location.href = '/booking'}>Book This Bike</button>
             </div>
